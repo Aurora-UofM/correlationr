081b468fba01c9031b82b8f409eab8a9a4ee3e94
Aurora-UofM <roberta5@myumanitoba.ca>
2025-04-07 13:44:30 -0500

    push

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Data collected for Swim Time
+#'
+#' Contains four continuous variables.
+#'
+#' @format A data frame with 200 rows and 4 variables:
+#'  \describe{
+#'    \item{Age}{The age of the person taking swim lessons}
+#'    \item{SwimTime}{The quantity of time the person spent swimming}
+#'    \item{Temp}{The temperature of the water during the swim lesson}
+#'    \item{UV}{The UV index during the swim lesson}
+#'    }
+#'
+#'    @source {Created in-house to serve as an example.}
+#'
+#'    @examples
+#'    data(SwimLessons)
+"SwimLessons"
